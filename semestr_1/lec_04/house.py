0ea9d33c3c6126653905f010f0fab85dc5bf65a7
Timofey Khirianov <tkhirianov@gmail.com>
2019-09-23 10:21:58 +0300

    Вызвал функцию рисования домика. Пока это -- функция-заглушка.

--- semestr_1/lec_04/house.py
+++ semestr_1/lec_04/house.py
@@ -1,7 +1,15 @@
-from graph.import *
+from graph import *
 
-def main()
+def main():
+    windowSize(600, 500)
+    paint_house(200, 200, 300, 400)
+    run()
+
+
+def paint_house(x, y, width, height):
+    """ Рисует домик
+        (x, y) -- опорная точка, левая-нижняя точка крыши
+    """
     pass
-
 
 main()
