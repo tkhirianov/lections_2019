428f5a71bfb28188360cee9f78b7c9ddffc4c6e9
Timofey Khirianov <tkhirianov@gmail.com>
2019-10-21 10:24:52 +0300

    Объектный рефакторинг
    1. Вычленил класс Ball
    2. Ответственность за положение, смещение и отображение возложена на этот класс
    3. Вписал вызов методов класса так, чтобы один шарик летал.

--- semestr_1/lec_08/my_balls.py
+++ semestr_1/lec_08/my_balls.py
@@ -4,38 +4,49 @@ from random import randint
 WIDTH = 300
 HEIGHT = 200
 
+
+class Ball:
+    def __init__(self):
+        self.R = randint(20, 50)
+        self.x = randint(self.R, WIDTH - self.R)
+        self.y = randint(self.R, HEIGHT - self.R)
+        self.dx, self.dy = (+2, +3)
+        self.ball_id = canvas.create_oval(self.x - self.R,
+                                     self.y - self.R,
+                                     self.x + self.R,
+                                     self.y + self.R, fill="green")
+
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+        if self.x + self.R > WIDTH or self.x - self.R <= 0:
+            self.dx = -self.dx
+        if self.y + self.R > HEIGHT or self.y - self.R <= 0:
+            self.dy = -self.dy
+
+    def show(self):
+        canvas.move(self.ball_id, self.dx, self.dy)
+
+
 def canvas_click_handler(event):
     print('Hello World! x=', event.x, 'y=', event.y)
 
-def tick():
-    global x, y, dx, dy
-    x += dx
-    y += dy
-    if x + R > WIDTH or x - R <= 0:
-        dx = -dx
-    if y + R > HEIGHT or y - R <= 0:
-        dy = -dy
 
-    canvas.move(ball_id, dx, dy)
+def tick():
+    ball.move()
+    ball.show()
     root.after(50, tick)
 
 
 def main():
-    global root, canvas
-    global ball_id, x, y, dx, dy, R  # TODO: сделать объекно-ориентированный рефакторинг
+    global root, canvas, ball
     
     root = tk.Tk()
     root.geometry(str(WIDTH) + "x" + str(HEIGHT))
     canvas = tk.Canvas(root)
     canvas.pack(anchor="nw", fill=tk.BOTH)
     canvas.bind('<Button-1>', canvas_click_handler)
-
-    R = randint(20, 50)
-    x = randint(R, WIDTH - R)
-    y = randint(R, HEIGHT - R)
-    dx, dy = (+2, +3)
-    ball_id = canvas.create_oval(x - R, y - R, x + R, y + R, fill="green")
-
+    ball = Ball()
     tick()
     root.mainloop()
 
