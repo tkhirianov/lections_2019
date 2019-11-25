b328f3fab1bb34dfca64b3c234b9f73cec55ce99
Timofey Khirianov <tkhirianov@gmail.com>
2019-11-25 10:26:08 +0300

    some tests

--- semestr_1/lec_12/mymodule.py
+++ semestr_1/lec_12/mymodule.py
@@ -8,8 +8,22 @@ non-negative numbers.
 """
 
 def factorial(n):
+    """Function to calculate the production of 1*2*...*n.
+    
+    >>> factorial(1)
+    1
+    >>> factorial(0)
+    1
+    >>> factorial(5)
+    120
+
+    """
     x = 1
     for i in range(1, n + 1):
-        x *= i
+        x = x + i
     return x
 
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()    
